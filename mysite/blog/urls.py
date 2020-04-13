ed5062d2f5a28ac57d2912570a9b29ddf3379031
NineMan <nelubinmv@inbox.ru>
2020-04-14 03:43:18 +0400

    Change to class-based views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('',                                                   views.post_list,   name='post_list'),
+    # path('',            views.post_list,   name='post_list'),  # Вариант на "общих" представлениях (views)
+    path('', views.PostListView.as_view(), name='post_list'),   # Вариант на Class-based views
     path('blog/<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger    # Для общего представления
+from django.views.generic import ListView                                   # Для представления на основе класса
 from .models import Post
 
 
-def post_list(request):
-    # posts = Post.objects.all()
-    # posts = Post.objects.filter(status='published')
-    object_list = Post.objects.filter(status='published')
-    paginator = Paginator(object_list, 3)               # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                   'posts': posts
-                   })
+class PostListView(ListView):
+    # Представление на основе классов
+    # queryset = Post.publish.all()
+    queryset = Post.objects.filter(status='published')
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(request):
+    # Предсталение (views) общее
+#     # posts = Post.objects.all()
+#     # posts = Post.objects.filter(status='published')
+#     object_list = Post.objects.filter(status='published')
+#     paginator = Paginator(object_list, 3)               # 3 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'page': page,
+#                    'posts': posts
+#                    })
 
 
 def post_detail(request, year, month, day, post):
