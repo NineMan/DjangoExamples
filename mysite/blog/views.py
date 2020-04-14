8c14e186f8e15554a8caca07a04c542a27afe50b
NineMan <nelubinmv@inbox.ru>
2020-04-14 04:02:36 +0400

    Light Commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 class PostListView(ListView):
     # Представление на основе классов
-    # queryset = Post.publish.all()
+    # queryset = Post.publish.all()     # Так на сайте предлагается фильтровать опубликованные посты
     queryset = Post.objects.filter(status='published')
     context_object_name = 'posts'
     paginate_by = 3
